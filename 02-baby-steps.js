a38c773dae95303d76c9da192ad3b51fb03cfa22
Imran Chaudhry <ichaudhry@gmail.com>
2015-04-13 17:58:16 +0100

    First two exercises done

---
+++ 02-baby-steps.js
@@ -0,0 +1,14 @@
+//console.log(process.argv);
+
+var total = 0;
+var argz = process.argv;
+
+argz.forEach(sumNumbers);
+
+function sumNumbers(element, index, array) {
+  if(index < 2) { return; }
+
+  total += Number(element);
+}
+
+console.log(total);
